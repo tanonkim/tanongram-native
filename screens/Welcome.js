a036e33b75f89facb63bceece6dd55b6eb5ac627
tanonkim <robbins4bos@gmail.com>
2022-05-27 16:00:31 +0900

    add : Moving screen with RN navigation

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
   return (
     <View>
       <Text>Welcome</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to Create Account</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <View>
+          <Text>Go to Login</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
