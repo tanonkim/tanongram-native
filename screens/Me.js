bccce536e797fc7a21176d13c4f11c4b4b239065
tanonkim <robbins4bos@gmail.com>
2022-05-30 18:21:15 +0900

    add : header domination

--- screens/Me.js
+++ screens/Me.js
@@ -1,7 +1,14 @@
 import { Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import useMe from "../hooks/useMe";
 
-export default function Me() {
+export default function Me({ navigation }) {
+  const { data } = useMe();
+  useEffect(() => {
+    navigation.setOptions({
+      title: data?.me?.username,
+    });
+  }, []);
   return (
     <View
       style={{
