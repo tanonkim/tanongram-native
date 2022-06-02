c3849b14a971833ab54afc03336b9ecbd93f63ed
tanonkim <robbins4bos@gmail.com>
2022-06-02 14:08:51 +0900

    add : Rooms Screen 1 (layout + connection)

--- navigators/MessagesNav.js
+++ navigators/MessagesNav.js
@@ -2,12 +2,24 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Room from "../screens/Room";
 import Rooms from "../screens/Rooms";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 export default function MessagesNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: "black",
+        },
+        headerLeft: ({ tintColor }) => (
+          <Ionicons color={tintColor} name="chevron-back-outline" size={28} />
+        ),
+      }}
+    >
       <Stack.Screen name="Rooms" component={Rooms} />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
